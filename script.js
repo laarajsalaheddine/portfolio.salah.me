cdbfec5e978d46e9931d0337610df0bcd9f20dea
salah <laarajsalaheddine@gmail.com>
2024-01-02 16:21:41 +0100

    contact form, responsiven not functional

--- script.js
+++ script.js
@@ -1,29 +1,18 @@
 document.addEventListener('DOMContentLoaded', function () {
     const downloadCVDropDown = document.getElementById('download-cv-dropdown-content');
     const downloadCV = document.getElementById('download-cv');
-    function handleScriptError(target) {
-        target.setAttribute("src", `./${target.getAttribute('data-library')}.min.js`);
-        target.setAttribute("crossorigin", "");
-        target.setAttribute("integrity", "");
-    }
-    function handleStyleTagError(target) {
-        target.setAttribute("href", `./${target.getAttribute('data-library')}.min.js`);
-        target.setAttribute("crossorigin", "");
-        target.setAttribute("integrity", "");
-    }
-
     document.addEventListener("click", function (event) {
         const navbarTogglerBtn = document.querySelector('.lse-navbar-toggler');
         const navbarTogglerNav = document.getElementById('navbarToggler');
-
         const isNavbarToggler = event.target.classList.contains('lse-navbar-toggler') ||
             event.target.parentNode.classList.contains('lse-navbar-toggler');
-
-        if (event.target.parentNode != downloadCV) {
-            downloadCVDropDown.classList.add("d-none");
-        }
+            
         navbarTogglerBtn.classList.toggle("change", isNavbarToggler);
         navbarTogglerNav.classList.toggle("show", isNavbarToggler);
+        
+        if(!(event.target.parentNode.id.trim() == "download-cv" || event.target.id.trim() == "download-cv")){
+            downloadCVDropDown.classList.add('d-none');   
+        }
     });
 
     document.querySelectorAll('li.nav-item').forEach(element => {
@@ -32,6 +21,8 @@ document.addEventListener('DOMContentLoaded', function () {
             element.classList.add("active");
         });
     });
+
+
 
     downloadCV.addEventListener("click", function (event) {
         downloadCVDropDown.classList.toggle('d-none');
